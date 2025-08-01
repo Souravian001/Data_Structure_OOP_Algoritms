ffabb7159d16bc167eb493d51b3c318648ee19f0
Sourav <roysourav1812@gmail.com>
2025-08-01 21:48:17 +0530

    pair in array

--- pair_in_array.java
+++ pair_in_array.java
@@ -3,9 +3,9 @@ public class pair_in_array{
 public static int pair_in_arr(int array[]){
     int tp = 0; // tp = total no. of pair which will be also returned by this funtion along with pair
     for (int i = 0; i < array.length; i++) {
-        int current = array[i];
+        //int current = array[i];
         for (int j = i+1; j < array.length; j++){// inner loop will give the next element of the pair & j=i+1 because it will fetch the value of next index of i
-            System.out.print("("+ current+","+ array[j]+")");
+            System.out.print("("+ array[i]+","+ array[j]+")");
             tp++;// will update tp after printing each pair
         }
         System.out.println(); // for next line for understanding properly
